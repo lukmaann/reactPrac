a309fd5fd3a2ff4fcd96d7734cc52f850ebfa065
LNUAKX <lukmaannadaf@gmail.com>
2023-09-09 19:32:31 +0530

    modify

---
+++ loginPassport/Server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app=express();
+
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+
+})
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server started successfully");
+})
